c272cd7c880042f83e6b215ff37c8810aa6977c2
Ahrcantos <ahrcantos99@gmail.com>
2023-08-20 03:28:41 -0700

    feat: basic tokenizer

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,5 +1,7 @@
+#[derive(Debug)]
 enum Error {
     UnknownKeyword(String),
+    NoMatch,
 }
 
 type Result<T> = std::result::Result<T, Error>;
@@ -8,13 +10,6 @@ type Result<T> = std::result::Result<T, Error>;
 struct Span {
     start: usize,
     end: usize,
-}
-
-impl Span {
-    pub fn offset(&mut self, value: usize) {
-        self.start += value;
-        self.end += value;
-    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -27,7 +22,13 @@ struct Token {
 enum TokenKind {
     Keyword(Keyword),
     Comma,
-    Space,
+    Equals,
+    GreaterThan,
+    GreaterThanEquals,
+    SmallerThan,
+    SmallerThanEquals,
+    Whitespace,
+    SemiColon,
     Identifier(String),
     String(String),
     Integer(u32),
@@ -39,6 +40,7 @@ enum Keyword {
     From,
     Insert,
     Values,
+    Where,
 }
 
 struct Tokenizer {}
@@ -47,28 +49,63 @@ impl Tokenizer {
     pub fn parse(input: &str) -> Vec<Token> {
         let mut output = Vec::new();
         let mut current_position = input;
+        let mut offset = 0;
 
         while current_position.len() > 0 {
-            if let Ok((rest, mut token)) = Self::parse_keyword(current_position) {
-                let offset = input.len() - current_position.len();
-                token.span.offset(offset);
-                output.push(token);
-
+            if let Ok((rest, token)) = Self::whitespace((offset, current_position)) {
                 current_position = rest;
-            } else {
-                current_position = &current_position[1..];
+                offset = token.span.end;
+                output.push(token);
+                continue;
+            }
+
+            if let Ok((rest, token)) = Self::special((offset, current_position)) {
+                current_position = rest;
+                offset = token.span.end;
+                output.push(token);
+                continue;
+            }
+
+            if let Ok((rest, token)) = Self::identifier((offset, current_position)) {
+                current_position = rest;
+                offset = token.span.end;
+                output.push(token);
+                continue;
+            }
+
+            if let Ok((rest ,token)) = Self::number((offset, current_position)) {
+                current_position = rest;
+                offset = token.span.end;
+                output.push(token);
+                continue;
+            }
+
+            match Self::keyword((offset, current_position)) {
+                Ok((rest, token)) => {
+                    current_position = rest;
+                    offset = token.span.end;
+                    output.push(token);
+                    continue;
+                }
+                Err(err) => {
+                    dbg!(err);
+                    break;
+                }
             }
         }
 
-        output
+        return output;
     }
 
-    fn parse_keyword(input: &str) -> Result<(&str, Token)> {
+    fn keyword((offset, input): (usize, &str)) -> Result<(&str, Token)> {
         if input.to_lowercase().starts_with("select") {
             return Ok((
                 &input[6..],
                 Token {
-                    span: Span { start: 0, end: 6 },
+                    span: Span {
+                        start: offset,
+                        end: offset + 6,
+                    },
                     kind: TokenKind::Keyword(Keyword::Select),
                 },
             ));
@@ -78,23 +115,167 @@ impl Tokenizer {
             return Ok((
                 &input[6..],
                 Token {
-                    span: Span { start: 0, end: 6 },
+                    span: Span {
+                        start: offset,
+                        end: offset + 6,
+                    },
                     kind: TokenKind::Keyword(Keyword::Insert),
                 },
             ));
         }
 
-        if input.to_lowercase().starts_with(" ") {
+        if input.to_lowercase().starts_with("from") {
             return Ok((
-                &input[1..],
+                &input[4..],
                 Token {
-                    span: Span { start: 0, end: 1 },
-                    kind: TokenKind::Space,
+                    span: Span {
+                        start: offset,
+                        end: offset + 4,
+                    },
+                    kind: TokenKind::Keyword(Keyword::From),
+                },
+            ));
+        }
+
+        if input.to_lowercase().starts_with("where") {
+            return Ok((
+                &input[5..],
+                Token {
+                    span: Span {
+                        start: offset,
+                        end: offset + 5,
+                    },
+                    kind: TokenKind::Keyword(Keyword::Where),
                 },
             ));
         }
 
         return Err(Error::UnknownKeyword(String::from("idk")));
+    }
+
+    fn whitespace((position, input): (usize, &str)) -> Result<(&str, Token)> {
+        let start = position;
+        let mut end = position;
+        let mut current = input;
+
+        for (i, char) in input.chars().enumerate() {
+            if i == 0 && !char.is_whitespace() {
+                return Err(Error::NoMatch);
+            }
+
+            if char.is_whitespace() {
+                current = &current[1..];
+                end += 1;
+            } else {
+                return Ok((
+                    current,
+                    Token {
+                        span: Span { start, end },
+                        kind: TokenKind::Whitespace,
+                    },
+                ));
+            }
+        }
+
+        return Ok((
+            current,
+            Token {
+                span: Span { start, end },
+                kind: TokenKind::Whitespace,
+            },
+        ));
+    }
+
+    fn special((position, input): (usize, &str)) -> Result<(&str, Token)> {
+
+        if &input[0..1] == "," {
+            return Ok((
+                &input[1..],
+                Token {
+                    span: Span { start: position, end: position + 1 },
+                    kind: TokenKind::Comma,
+                },
+            ))
+        }
+
+        if &input[0..1] == "=" {
+            return Ok((&input[1..], Token {
+                span: Span { start: position, end: position + 1 },
+                kind: TokenKind::Equals,
+            }))
+        }
+
+        if &input[0..1] == ";" {
+            return Ok((&input[1..], Token {
+                span: Span { start: position, end: position + 1 },
+                kind: TokenKind::SemiColon,
+            }))
+        }
+
+        return Err(Error::NoMatch);
+    }
+
+    fn identifier((position, input): (usize, &str)) -> Result<(&str, Token)> {
+        let mut iden = String::new();
+        let mut current_position = input;
+        let start = position;
+        let mut end = position;
+
+        for (i, char) in input.chars().enumerate() {
+            if i == 0 && char != '\'' {
+                return Err(Error::NoMatch);
+            }
+
+            end += 1;
+            current_position = &current_position[1..];
+
+            if i != 0 && char == '\'' {
+                return Ok((
+                    current_position,
+                    Token {
+                        span: Span { start, end },
+                        kind: TokenKind::Identifier(iden),
+                    },
+                ));
+            }
+
+            if char.is_alphabetic() || char == '.' {
+                iden.push(char)
+            }
+        }
+
+        return Err(Error::NoMatch);
+    }
+
+    fn number((position, input): (usize, &str)) -> Result<(&str, Token)> {
+
+        let mut number = String::new();
+        let start = position;
+        let mut end = position;
+        let mut current = input;
+
+        for (i, char) in input.chars().enumerate() {
+            if i == 0 && !char.is_numeric() {
+                return Err(Error::NoMatch);
+            }
+
+
+            if !char.is_numeric() {
+                break;
+            }
+
+            end += 1;
+            current = &current[1..];
+
+            number.push(char);
+        }
+
+        let n: u32 = number.parse().expect("Failed to parse number. Probably too big");
+
+        return Ok((current, Token {
+            span: Span { start, end },
+            kind: TokenKind::Integer(n),
+        }));
     }
 }
 
@@ -117,7 +298,7 @@ mod tests {
                 },
                 Token {
                     span: Span { start: 6, end: 7 },
-                    kind: TokenKind::Space,
+                    kind: TokenKind::Whitespace,
                 },
                 Token {
                     span: Span { start: 7, end: 13 },
@@ -128,9 +309,8 @@ mod tests {
     }
 
     #[test]
-    fn test_tokenize_iden() {
-        let input = "Select 'id'";
-
+    fn test_tokenize_comma() {
+        let input = "Select,Insert";
         let tokens = Tokenizer::parse(input);
 
         assert_eq!(
@@ -142,13 +322,114 @@ mod tests {
                 },
                 Token {
                     span: Span { start: 6, end: 7 },
-                    kind: TokenKind::Space,
+                    kind: TokenKind::Comma,
                 },
                 Token {
-                    span: Span { start: 7, end: 10 },
+                    span: Span { start: 7, end: 13 },
+                    kind: TokenKind::Keyword(Keyword::Insert)
+                },
+            ]
+        )
+    }
+
+    #[test]
+    fn test_tokenize_iden() {
+        let input = "Select   'id'";
+
+        let tokens = Tokenizer::parse(input);
+
+        assert_eq!(
+            tokens,
+            vec![
+                Token {
+                    span: Span { start: 0, end: 6 },
+                    kind: TokenKind::Keyword(Keyword::Select)
+                },
+                Token {
+                    span: Span { start: 6, end: 9 },
+                    kind: TokenKind::Whitespace,
+                },
+                Token {
+                    span: Span { start: 9, end: 13 },
                     kind: TokenKind::Identifier(String::from("id")),
                 }
             ]
         );
+    }
+
+    #[test]
+    fn test_tokenize_select_with_where() {
+        let input = "SELECT 'id' FROM 'users' WHERE 'id' = 2;";
+        let tokens = Tokenizer::parse(input);
+
+        assert_eq!(
+            tokens,
+            vec![
+                Token {
+                    span: Span { start: 0, end: 6 },
+                    kind: TokenKind::Keyword(Keyword::Select)
+                },
+                Token {
+                    span: Span { start: 6, end: 7 },
+                    kind: TokenKind::Whitespace,
+                },
+                Token {
+                    span: Span { start: 7, end: 11 },
+                    kind: TokenKind::Identifier(String::from("id")),
+                },
+                Token {
+                    span: Span { start: 11, end: 12 },
+                    kind: TokenKind::Whitespace,
+                },
+                Token {
+                    span: Span { start: 12, end: 16 },
+                    kind: TokenKind::Keyword(Keyword::From),
+                },
+                Token {
+                    span: Span { start: 16, end: 17 },
+                    kind: TokenKind::Whitespace,
+                },
+                Token {
+                    span: Span { start: 17, end: 24 },
+                    kind: TokenKind::Identifier(String::from("users")),
+                },
+                Token {
+                    span: Span { start: 24, end: 25 },
+                    kind: TokenKind::Whitespace,
+                },
+                Token {
+                    span: Span { start: 25, end: 30 },
+                    kind: TokenKind::Keyword(Keyword::Where),
+                },
+                Token {
+                    span: Span { start: 30, end: 31 },
+                    kind: TokenKind::Whitespace,
+                },
+                Token {
+                    span: Span { start: 31, end: 35 },
+                    kind: TokenKind::Identifier(String::from("id")),
+                },
+                Token {
+                    span: Span { start: 35, end: 36 },
+                    kind: TokenKind::Whitespace,
+                },
+                Token {
+                    span: Span { start: 36, end: 37 },
+                    kind: TokenKind::Equals,
+                },
+                Token {
+                    span: Span { start: 37, end: 38 },
+                    kind: TokenKind::Whitespace,
+                },
+                Token {
+                    span: Span { start: 38, end: 39 },
+                    kind: TokenKind::Integer(2),
+                },
+                Token {
+                    span: Span { start: 39, end: 40 },
+                    kind: TokenKind::SemiColon,
+                },
+            ]
+        )
     }
 }
